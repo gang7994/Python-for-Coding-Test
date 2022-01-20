ed5f8471e4bf44fe7a5c3202efe4c8831aef544a
gang7994 <gang7994@naver.com>
2022-01-20 17:15:35 +0900

    Fix code

--- Chapter.09(Shortest_Path)/Floyd.py
+++ Chapter.09(Shortest_Path)/Floyd.py
@@ -6,7 +6,10 @@ graph = [[INF] * (n+1) for i in range(n+1)]
 
 for i in range(m):
     a,b,c = map(int, input().split())
-    graph[a][b] = c
+    if graph[a][b] != INF:
+        graph[a][b] = min(graph[a][b], c)
+    else:
+        graph[a][b] = c
     
 for i in range(1,n+1):
     for j in range(1,n+1):
